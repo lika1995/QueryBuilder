901c9b3a3103329da9551ac618bb1d204803d89e
Lika Yan <likusya865@gmail.com>
2020-07-06 04:10:45 +0600

    исправила README.md

---
+++ QueryBuilder.php
@@ -0,0 +1,113 @@
+<?php
+
+class QueryBuilder
+{
+    private $pdo;
+
+    /**
+     * QueryBuilder constructor.
+     * @param $pdo
+     *
+     * записывает в $pdo объект класса PDO
+     */
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @param $table
+     * @return mixed
+     *
+     * Получает все записи из тфблицы в БД
+     */
+    public function getAll($table)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM {$table}");
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param $table
+     * @param $data
+     *
+     * Создает новую запись в БД
+     */
+    public function create($table, $data)
+    {
+        $keys = implode(',', array_keys($data));
+        $values = '';
+
+        foreach($data as $field){
+            $values .= '?,';
+        }
+        $tags = rtrim($values, ',');
+        $sql = "INSERT INTO {$table} ({$keys}) VALUES ({$tags})";
+        $stmt = $this->pdo->prepare($sql);
+        if(count($data)){
+            $i = 1;
+            foreach ($data as $param) {
+                $stmt->bindValue($i, $param);
+                $i++;
+            }
+        }
+
+        $stmt->execute();
+    }
+
+    /**
+     * @param $table
+     * @param $id
+     * @return mixed
+     *
+     * Получает запись из таблицы по id
+     */
+    public function getOneById($table, $id)
+    {
+        $sql = "SELECT * FROM {$table} WHERE id=?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1 , $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param $table
+     * @param array $data
+     * @param $id
+     *
+     * Меняет запись в таблице по id
+     */
+    public function update($table,array $data, $id)
+    {
+        $keys = array_keys($data);
+        $string = '';
+
+        foreach ($keys as $key){
+            $string .= $key . '=:' . $key . ',';
+        }
+
+        $keys = rtrim($string, ',');
+        $data['id'] = $id;
+        $sql = "UPDATE {$table} SET {$keys} WHERE id=:id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute($data);
+    }
+
+    /**
+     * @param $table
+     * @param $id
+     *
+     * Удаляет запись из таблицы
+     */
+    public function delete($table, $id)
+    {
+        $sql = "DELETE FROM {$table} WHERE id=:id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
+}
--- README.md
+++ README.md
@@ -3,11 +3,31 @@
 Query Builder is a fast, simple, methods-chaining, dependency-free library to create 
 SQL Queries simple and fast to write, extend and manage.
 
-##Installation
+## Installation
 
-Download QueryBuilder.php than save it to your project directory.
+add to your file composer.json this settings
+```json
+ "name": "yourProjectName",
+    "description": "Your description",
+    "type": "your type",
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/lika1995/querybuilder"
+        }
+    ],
+    "require": {
+        "lika1995/querybuilder": "dev-master"
+    },
+    "minimum-stability": "dev"
+}
+```
+Add this package to your composer.
 
-##Usage
+composer require lika1995/querybuilder
+
+
+## Usage
 ```php
 <?php
 include 'QueryBuilder.php';
@@ -15,19 +35,19 @@ include 'QueryBuilder.php';
 $connection = new PDO('dns...');
 $qb = new QueryBuilder($connection);
 
-//Select all
+// Select all
 
 $qb->getAll('table_name');
 
-//Insert
+// Insert
 
 $qb->create('table_name', ['name' => 'Adam', 'age' => '...']);
 
-//Select one by id
+// Select one by id
 
 $qb->getOneById('table_name', $id);
 
-//Update
+// Update
 
 $qb->update('table_name', ['name' => 'Alex'], $id);
 
